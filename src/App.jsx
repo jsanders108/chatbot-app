16a5fa483d1d39c7ba480a1ed751c9dd79726dde
jsanders108 <74698177+jsanders108@users.noreply.github.com>
2023-05-16 10:58:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { useState, useRef, useEffect } from "react";
-import logoImage from "/chatbot-logo.png"
-
+import logoImage from "./chatbot-logo.png"
 
 function App() {
   const [promptInput, setPromptInput] = useState("");
